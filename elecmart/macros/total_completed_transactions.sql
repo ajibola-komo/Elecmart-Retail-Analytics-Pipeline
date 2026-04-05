ec8b7002da3be584191ac8b045a3cb715f5bc365
ajibola-komo <ajibolaokomolafe@gmail.com>
2026-04-05 11:18:44 +0100

    updated gold layer tests

--- elecmart/macros/total_completed_transactions.sql
+++ elecmart/macros/total_completed_transactions.sql
@@ -4,7 +4,7 @@ select *
 from {{ model }}
 where total_completed_transactions != (
     select coalesce(count(transaction_id), 0)
-    from {{ ref('get_rev_and_cost') }} 
+    from {{ ref('silver_fact_transaction') }}  where transaction_status = 'Completed'
 )
 
 {% endtest %}
--- elecmart/macros/total_cost_reconciliation.sql
+++ elecmart/macros/total_cost_reconciliation.sql
@@ -3,7 +3,7 @@
 select *
 from {{ model }}
 where total_cost_for_completed_transactions != (
-    select total_completed_cost from {{ ref('get_rev_and_cost') }}
+    select total_cost from {{ ref('get_rev_and_cost') }}
 )
 
 {% endtest %}
